2c297144c65357431feffdb3a419173219b3c017
Janani <jananis@atatus.com>
2025-07-22 11:14:02 +0530

    added eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
+// eslint.config.mjs
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest"
+    },
+    plugins: {
+      react
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+];
